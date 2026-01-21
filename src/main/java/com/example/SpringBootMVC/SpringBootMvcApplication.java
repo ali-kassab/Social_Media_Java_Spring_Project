81956cad86515a3229cec011159a1e77bab5c110
ali-kassab <alikassab155@gmail.com>
2026-01-22 01:42:05 +0200

    reset

--- src/main/java/com/example/SpringBootMVC/SpringBootMvcApplication.java
+++ src/main/java/com/example/SpringBootMVC/SpringBootMvcApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootMvcApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMvcApplication.class, args);
 	}
